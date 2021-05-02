d8b38ddde7c80ffa53be104c9eceb9b9d499da9a
Hernan Yupanqui <yup19001@byui.edu>
2021-05-01 21:29:44 -0500

    week 02

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+const readRoute = require("./routes/bookinfo");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(readRoute);
+
+
+app.use("/", (req, res, next) => {
+    console.log(req.url);
+    res.status(404);
+    res.render("404", {
+        title: "404 Page not found"
+    });
+});
+
+
+const portListening = 3000;
+app.listen(portListening, () => console.log("Listening on: ", portListening));
---
+++ routes/bookinfo.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const fs = require("fs");
+
+const router = express.Router();
+
+router.get("/bookinfo", (req, res, next) => {
+    res.render("bookinfo-write", {title: "Record Books information"});
+});
+
+router.post("/bookinfo", (req, res, next) => {
+    console.log("recibing ino-");
+    console.log(JSON.stringify(req.body));
+    
+    fs.readFile("./database.json", "utf-8", (error, data) => {
+        const the_data = JSON.parse(data);
+        the_data.push(req.body);
+        
+        fs.writeFile("./database.json", JSON.stringify(the_data), (err) => {
+            console.log(err);
+        }); 
+    });
+
+    
+    res.render("booksinfo-read", {
+        title: "Reading books",
+        booksinfo: [{book_title: req.body.book_title
+            , book_summary: req.body.book_summary
+        }]
+    })
+});
+
+router.get("/bookinfo/all", (req, res, next) => {
+    let all_data; 
+    fs.readFile("./database.json", "utf-8", (error, data) => {
+        all_data = JSON.parse(data);
+        
+        res.render("booksinfo-read", {
+            title: "All info",
+            booksinfo: all_data
+        });
+    });
+    
+});
+
+module.exports = router;
