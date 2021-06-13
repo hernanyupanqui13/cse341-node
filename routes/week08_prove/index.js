a2aee1d111c353228e348e1506e417fe3c5b574d
Hernan <hernan.yupanqui.prieto@gmail.com>
2021-06-13 00:04:26 -0500

    improving route name

--- routes/week08_prove/index.js
+++ routes/week08_prove/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const week08_controller = require("../../controllers/week08_prove/productsContoller");
 
-router.get("/test", week08_controller.getProducts);
+router.get("/week08", week08_controller.getProducts);
 
 
 
