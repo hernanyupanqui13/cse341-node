ee4d3e2986ca11b6a815cb3332fc2d52c02313a4
Hernan <hernan.yupanqui.prieto@gmail.com>
2021-06-21 13:53:04 -0500

    adding more features to the pagination

--- public/js/week09_script.js
+++ public/js/week09_script.js
@@ -1,6 +1,7 @@
 console.log("hola!");
 
-
+const lastButton = document.getElementById("lastButton");
+const firstButton = document.getElementById("firstButton");
 const nextButton = document.getElementById("nextButton");
 const previousButton = document.getElementById("previousButton");
 const parentElement = document.getElementById("pokemon-list");
@@ -8,20 +9,34 @@ const parentElement = document.getElementById("pokemon-list");
 
 nextButton.addEventListener("click", next => {
   console.log("clickin next ");
-  nextData = JSON.parse(nextButton.dataset.nextData);
+  nextData = JSON.parse(nextButton.dataset.pageData);
   changePagePokemons(nextData);
 });
 
 previousButton.addEventListener("click", event => {
-
   console.log("clickin next ");
-  prevData = JSON.parse(previousButton.dataset.prevData);
+  prevData = JSON.parse(previousButton.dataset.pageData);
   changePagePokemons(prevData);
-  
+});
+
+firstButton.addEventListener("click", event => {
+  console.log("clicking first");
+  firstData = JSON.parse(firstButton.dataset.pageData);
+  changePagePokemons(firstData);
+});
+
+
+lastButton.addEventListener("click", event => {
+  console.log("clicking last");
+  lastData = JSON.parse(lastButton.dataset.pageData);
+  changePagePokemons(lastData);
 });
 
 getPokemons()
-.then( data => renderData(data));  
+.then( data => {
+  renderData(data);
+  renderPagination(data);
+});  
 
 
 async function getPokemons(offset=0, limit=10) {
@@ -47,6 +62,7 @@ async function changePagePokemons(paginatonData) {
   const pokeData = await getPokemons(paginatonData.offset, paginatonData.limit);
 
   renderData(pokeData);
+  renderPagination(pokeData);
 }
 
 function renderData(data) {
@@ -65,13 +81,14 @@ function renderData(data) {
 
 
   // Render Next and Previo Buttons
+
   if (data.next === null) {
     nextButton.parentNode.classList.add("disabled");
   } else {
     nextButton.parentNode.classList.remove("disabled");
     const nextData = obtainParameters(data.next);
     console.log("not null next");
-    nextButton.dataset.nextData = JSON.stringify(nextData);
+    nextButton.dataset.pageData = JSON.stringify(nextData);
 
   }
 
@@ -80,11 +97,94 @@ function renderData(data) {
   } else {
     previousButton.parentNode.classList.remove("disabled");
     const prevData = obtainParameters(data.previous);
-    previousButton.dataset.prevData = JSON.stringify(prevData);
+    previousButton.dataset.pageData = JSON.stringify(prevData);
     console.log("not null prev");
   }
 
 
+}
+
+function renderPagination(data) {
+  let totalPages = 0;
+  let paginationData = {};
+  let currentPage = 0;
+  
+  if (data.next !== null) {
+    paginationData = obtainParameters(data.next);  
+    console.log(paginationData, "pag data");
+    currentPage = (paginationData.offset/10);
+    //currentPage = currentPage == 0 ? 1: currentPage; 
+    totalPages = Math.ceil(data.count / 10);
+    console.log(totalPages, currentPage, "not null");
+  } else {
+    paginationData = obtainParameters(data.previous);
+    totalPages = Math.ceil(data.count / paginationData.limit);
+    currentPage = totalPages;
+    console.log(totalPages, currentPage, "null");
+  }
+  
+
+  lastButton.dataset.pageData = JSON.stringify({
+    offset: (totalPages - 1)*10,
+    limit: 10
+  });
+
+  firstButton.dataset.pageData =  JSON.stringify({
+    offset:0,
+    limit: 10
+  });
+
+  firstInPagination = currentPage - 2;
+  lastInPagination = currentPage + 3
+
+  if (currentPage < 3) {
+    firstInPagination = 1;
+    lastInPagination = 6;
+  } 
+  
+
+  if (currentPage > totalPages - 3) {
+    firstInPagination = totalPages - 5;
+    lastInPagination = totalPages;
+  } 
+
+  let pagesHtml = "";
+  let index = firstInPagination;
+
+  while (index < lastInPagination+1) {
+    console.log("CURRENT AND ACTIVE", currentPage, "a", index);
+    pagesHtml += `
+      <li class="page-item tempPages ${index==currentPage ? 'active': ''}">
+        <a class="page-link" href="#" tabindex="-1" 
+          aria-disabled="true" 
+          data-page-data=${JSON.stringify({offset: (index-1)*10, limit: 10})}>
+          ${index}
+        </a>
+      </li>
+    `;
+
+    index++;
+  }
+  const parentHtml = document.querySelector(".pagination-ul");
+  
+  // Removing the unsued pages
+  [...parentHtml.querySelectorAll(".tempPages")].forEach( element => {
+    console.log("removing temp pages");
+    parentHtml.removeChild(element)
+  });
+
+  const beforeInjecting = document.querySelector(".beforeInjecting");
+  beforeInjecting.insertAdjacentHTML("beforebegin", pagesHtml);
+  addClickEventToPages();
+}
+
+function addClickEventToPages() {
+  [...document.querySelectorAll(".tempPages")].forEach( element => {
+    element.addEventListener("click", event => {
+      pageData = JSON.parse(event.target.dataset.pageData);
+      changePagePokemons(pageData); 
+    });
+  });
 }
 
 
